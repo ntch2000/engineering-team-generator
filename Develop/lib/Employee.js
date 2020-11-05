a33bbbd5343cdac3336b6a27ba1aa4bc014c9236
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-11-05 09:37:56 -0500

    Completed initial code for Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,33 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+
+    // currently hardcoded role of employee
+    this.role = "Employee";
+  }
+
+  // returns the name of the employee
+  getName() {
+    return this.name;
+  }
+
+  // returns the id of the employee
+  getId() {
+    return this.id;
+  }
+
+  // returns the email of the employee
+  getEmail() {
+    return this.email;
+  }
+
+  // returns the role of the employee
+  getRole() {
+    return this.role;
+  }
+}
+
+module.exports = Employee;
